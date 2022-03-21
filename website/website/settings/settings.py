16f11b010afed671532949024ac90cb4b1461c45
Angel Baro Noguero <angelbaro2001@gmail.com>
2022-03-21 19:03:44 +0100

    static

--- website/website/settings/settings.py
+++ website/website/settings/settings.py
@@ -118,8 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR
+STATICFILES_DIRS = (os.path.join('static'),)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
